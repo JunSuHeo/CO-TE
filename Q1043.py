e4070caf800081d71444d048c89c0f0a816ba993
JSHeo <jsheo@hyundai-ite.com>
2021-12-27 00:15:30 +0900

    백준 1043

---
+++ Q1043.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+arr = set(input().rstrip().split()[1:])
+
+parties = []
+res = []
+
+for _ in range(M):
+    parties.append(set(input().rstrip().split()[1:]))
+    res.append(1)
+
+
+for _ in range(M):
+    for i, party in enumerate(parties):
+        if not arr.isdisjoint(party):
+            res[i] = 0
+            arr.update(party)
+
+print(sum(res))
